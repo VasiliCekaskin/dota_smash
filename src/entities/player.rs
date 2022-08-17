6f43bdd378de3194429f1565f6667eca0ef748de
Vasili Cekaskin <vasili.cekaskin@invision.de>
2022-08-17 15:16:13 +0200

    Extract player module

--- src/entities/player.rs
+++ src/entities/player.rs
@@ -1,1 +1,129 @@
+use bevy::{
+    input::keyboard::KeyboardInput,
+    prelude::{
+        AssetServer, Commands, Component, EventReader, EventWriter, KeyCode,
+        Plugin, Query, Res, Transform, Vec2, With,
+    },
+    sprite::SpriteBundle,
+    transform::TransformBundle,
+};
+use bevy_rapier2d::prelude::{Collider, GravityScale, LockedAxes, RigidBody};
 
+use crate::{GameConfig, GameState};
+
+pub struct PlayerPlugin;
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.add_event::<MovePlayerEvent>()
+            .add_startup_system(setup)
+            .add_system(keyboard_events)
+            .add_system(move_player_event_system)
+            .add_system(move_player_system);
+    }
+}
+
+#[derive(Component)]
+struct Player {
+    id: usize,
+    acceleration: Vec2,
+}
+
+struct MovePlayerEvent {
+    player_id: usize,
+    translation: Vec2,
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    game_config: Res<GameConfig>,
+    game_state: Res<GameState>,
+) {
+    if game_config.player_max_count > game_state.current_player_count {
+        commands
+            .spawn()
+            .insert(Player {
+                id: 1,
+                acceleration: Vec2 { x: 0.0, y: 0.0 },
+            })
+            .insert(RigidBody::Dynamic)
+            .insert(GravityScale(10.0))
+            .insert(LockedAxes::ROTATION_LOCKED)
+            .insert(Collider::capsule(
+                Vec2::new(0.0, 10.0),
+                Vec2::new(0.0, 0.0),
+                50.0,
+            ))
+            .insert_bundle(SpriteBundle {
+                texture: asset_server.load("axe_idle.png"),
+                ..Default::default()
+            })
+            .insert_bundle(TransformBundle::from(Transform::from_xyz(
+                0.0, 0.0, 0.0,
+            )));
+    }
+}
+
+fn keyboard_events(
+    mut keyboard_input_evr: EventReader<KeyboardInput>,
+    mut move_player_event: EventWriter<MovePlayerEvent>,
+    game_config: Res<GameConfig>,
+) {
+    use bevy::input::ButtonState;
+
+    for keyboard_input in keyboard_input_evr.iter() {
+        match keyboard_input.state {
+            ButtonState::Pressed => match keyboard_input.key_code {
+                Some(KeyCode::A) => move_player_event.send(MovePlayerEvent {
+                    player_id: 1,
+                    translation: Vec2 {
+                        x: -game_config.player_movement_translation_vector.x
+                            * game_config.player_acceleration.x,
+                        y: 0.0,
+                    },
+                }),
+                Some(KeyCode::D) => move_player_event.send(MovePlayerEvent {
+                    player_id: 1,
+                    translation: Vec2 {
+                        x: game_config.player_movement_translation_vector.x
+                            * game_config.player_acceleration.x,
+                        y: 0.0,
+                    },
+                }),
+                _ => (),
+            },
+            ButtonState::Released => match keyboard_input.key_code {
+                Some(KeyCode::A) => move_player_event.send(MovePlayerEvent {
+                    player_id: 1,
+                    translation: Vec2 { x: 0.0, y: 0.0 },
+                }),
+                Some(KeyCode::D) => move_player_event.send(MovePlayerEvent {
+                    player_id: 1,
+                    translation: Vec2 { x: 0.0, y: 0.0 },
+                }),
+                _ => (),
+            },
+        }
+    }
+}
+
+fn move_player_event_system(
+    mut move_player_event_evr: EventReader<MovePlayerEvent>,
+    mut query: Query<&mut Player>,
+) {
+    for move_player_event in move_player_event_evr.iter() {
+        for (mut player) in query.iter_mut() {
+            if move_player_event.player_id == player.id {
+                player.acceleration = move_player_event.translation;
+            }
+        }
+    }
+}
+
+// TODO move the player according to player acceleration
+fn move_player_system(mut query: Query<(&Player, &mut Transform)>) {
+    for (player, mut transform) in query.iter_mut() {
+        transform.translation.x += player.acceleration.x;
+        transform.translation.y += player.acceleration.y;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,59 @@
 use bevy_rapier2d::prelude::*;
-use std::{ptr::null, time::Duration};
 
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
-    input::{gamepad::GamepadButton, keyboard::KeyCode, Input},
     prelude::*,
-    render::render_resource::{
-        DepthStencilState, RenderPassDepthStencilAttachment,
-        RenderPassDescriptor,
-    },
-    window::WindowMode,
 };
-use iyes_loopless::prelude::FixedTimestepStage;
 
 mod entities;
-use entities::player;
+use entities::player::PlayerPlugin;
 
 const WINDOW_WIDTH: f32 = 1920.0;
 const WINDOW_HEIGHT: f32 = 1080.0;
 
-const GAME_LOOP_UPDATE_RATE: u64 = 10;
-const PLAYER_ACCELERATION: f32 = 1.0;
-const PLAYER_DECELERATION: f32 = 2.0;
-const PLAYER_MAX_SPEED: f32 = 5.0;
-const PLAYER_MAX_JUMP_SPEED: f32 = 40.0;
-const PLAYER_JUMP_DECELERATION: f32 = 3.0;
+////////////////////////////////////////////////////////////////
+// This must come into it's own modules later
+
+const PLAYER_MAX_COUNT: u32 = 2;
+const PLAYER_MOVEMENT_TRANSLATION_VECTOR: Vec2 = Vec2 { x: 1.0, y: 1.0 };
+const PLAYER_MAX_MOVEMENT_SPEED: f32 = 20.0;
+const PLAYER_ACCELERATION: Vec2 = Vec2 {
+    x: PLAYER_MAX_MOVEMENT_SPEED,
+    y: 0.0,
+};
+
+struct GameConfig {
+    player_max_count: u32,
+    player_movement_translation_vector: Vec2,
+    player_max_movement_speed: f32,
+    player_acceleration: Vec2,
+}
+impl Default for GameConfig {
+    fn default() -> Self {
+        Self {
+            player_max_count: PLAYER_MAX_COUNT,
+            player_movement_translation_vector:
+                PLAYER_MOVEMENT_TRANSLATION_VECTOR,
+            player_max_movement_speed: PLAYER_MAX_MOVEMENT_SPEED,
+            player_acceleration: PLAYER_ACCELERATION,
+        }
+    }
+}
+
+struct GameState {
+    current_player_count: u32,
+}
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            current_player_count: 0,
+        }
+    }
+}
+////////////////////////////////////////////////////////////////
 
 #[derive(Component)]
 struct Platform;
-
-#[derive(Component)]
-struct Player {
-    id: usize,
-}
 
 #[derive(Component)]
 struct Speed {
@@ -86,14 +107,6 @@ struct ViewDirection {
 pub const LAUNCHER_TITLE: &str = "Bevy Shell - Template";
 
 pub fn app() -> App {
-    let mut game_loop = SystemStage::parallel();
-
-    game_loop.add_system(teleport_wall_system);
-    game_loop.add_system(move_player_system);
-
-    // DEV tools
-    game_loop.add_system(_dev_tp_player_to_start_system);
-
     let mut app = App::new();
 
     app.insert_resource(WindowDescriptor {
@@ -105,22 +118,16 @@ pub fn app() -> App {
         ..Default::default()
     })
     .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+    .insert_resource(GameState::default())
+    .insert_resource(GameConfig::default())
     .add_plugins(DefaultPlugins)
     .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(50.0))
     .add_plugin(RapierDebugRenderPlugin::default())
-    .add_startup_system(setup)
-    .add_system(bevy::window::close_on_esc)
-    .add_system(keyboard_input_system)
-    .add_system(gamepad_system)
-    .add_system(move_camera)
-    .add_stage_before(
-        CoreStage::Update,
-        "game_loop",
-        FixedTimestepStage::new(Duration::from_millis(GAME_LOOP_UPDATE_RATE))
-            .with_stage(game_loop),
-    )
     .add_plugin(LogDiagnosticsPlugin::default())
     .add_plugin(FrameTimeDiagnosticsPlugin::default())
+    .add_startup_system(setup)
+    .add_plugin(PlayerPlugin)
+    .add_system(bevy::window::close_on_esc)
     .run();
 
     return app;
@@ -136,36 +143,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         .insert(Collider::cuboid(WINDOW_WIDTH, 50.0))
         .insert_bundle(TransformBundle::from(Transform::from_xyz(
             0.0, 2200.0, 0.0,
-        )));
-
-    // Spawn player
-    commands
-        .spawn()
-        .insert(Player { id: 1 })
-        .insert(ViewDirection {
-            value: ViewDirections::Right,
-        })
-        .insert(Statuses {
-            value: vec![StatusTypes::Idle],
-        })
-        .insert(Speed { value: 0.0 })
-        .insert(JumpSpeed {
-            value: PLAYER_MAX_JUMP_SPEED,
-        })
-        .insert(RigidBody::Dynamic)
-        .insert(GravityScale(10.0))
-        .insert(LockedAxes::ROTATION_LOCKED)
-        .insert(Collider::capsule(
-            Vec2::new(0.0, 10.0),
-            Vec2::new(0.0, 0.0),
-            50.0,
-        ))
-        .insert_bundle(SpriteBundle {
-            texture: asset_server.load("axe_idle.png"),
-            ..default()
-        })
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(
-            0.0, 0.0, 0.0,
         )));
 
     // Platform
@@ -220,274 +197,4 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         .insert_bundle(TransformBundle::from(Transform::from_xyz(
             400.0, -50.0, 0.0,
         )));
-}
-
-fn _dev_tp_player_to_start_system(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Transform)>,
-) {
-    if (keyboard_input.just_pressed(KeyCode::R)) {
-        for (_, mut transform) in &mut query {
-            transform.translation.x = 0.0;
-            transform.translation.y = 0.0;
-        }
-    }
-}
-
-fn teleport_wall_system(mut query: Query<(&Player, &mut Transform)>) {
-    for (_, mut transform) in &mut query {
-        if transform.translation.x <= -WINDOW_WIDTH / 2.0 {
-            transform.translation.x = WINDOW_WIDTH / 2.0;
-        }
-
-        if transform.translation.x >= WINDOW_WIDTH / 2.0 {
-            transform.translation.x = -WINDOW_WIDTH / 2.0;
-        }
-    }
-}
-
-fn move_player_system(
-    mut query: Query<(
-        &Player,
-        &mut Transform,
-        &mut Speed,
-        &mut Statuses,
-        &ViewDirection,
-        &mut JumpSpeed,
-    )>,
-) {
-    for (
-        _,
-        mut transform,
-        mut speed,
-        mut statuses,
-        view_direction,
-        mut jump_speed,
-    ) in &mut query
-    {
-        if statuses.value.contains(&StatusTypes::Jumping) {
-            if (jump_speed.value > 0.0) {
-                jump_speed.value -= PLAYER_JUMP_DECELERATION;
-            } else if jump_speed.value <= 0.0 {
-                // Don't jump anymore
-                jump_speed.value = PLAYER_MAX_JUMP_SPEED;
-                statuses.remove_status(&StatusTypes::Jumping);
-            }
-
-            transform.translation.y += jump_speed.value;
-        }
-
-        if statuses.value.contains(&StatusTypes::RunLeft)
-            || statuses.value.contains(&StatusTypes::RunRight)
-        {
-            // Accelerate the player
-            if speed.value <= PLAYER_MAX_SPEED {
-                speed.value += PLAYER_ACCELERATION
-            }
-        } else {
-            if speed.value > 0.0 {
-                let new_value = speed.value - PLAYER_DECELERATION;
-
-                if new_value < 0. {
-                    speed.value = 0.;
-                } else {
-                    speed.value = new_value;
-                }
-            }
-        }
-
-        let move_speed = (speed.value * speed.value).sqrt();
-
-        if view_direction.value == ViewDirections::Left {
-            transform.translation.x -= move_speed;
-        } else if view_direction.value == ViewDirections::Right {
-            transform.translation.x += move_speed;
-        }
-    }
-}
-
-fn keyboard_input_system(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Player, &mut Statuses, &mut ViewDirection, &mut Sprite)>,
-) {
-    for (player, mut statuses, mut view_direction, mut sprite) in &mut query {
-        if player.id == 1 {
-            if keyboard_input.just_pressed(KeyCode::W) {
-                if statuses.value.contains(&StatusTypes::Jumping) {
-                    return;
-                }
-
-                statuses.value.push(StatusTypes::Jumping);
-            }
-            if keyboard_input.just_pressed(KeyCode::A) {
-                sprite.flip_x = false;
-                view_direction.value = ViewDirections::Left;
-                statuses.value.push(StatusTypes::RunLeft);
-            }
-            if keyboard_input.just_pressed(KeyCode::D) {
-                sprite.flip_x = true;
-                view_direction.value = ViewDirections::Right;
-                statuses.value.push(StatusTypes::RunRight);
-            }
-            if keyboard_input.just_released(KeyCode::A) {
-                statuses.remove_status(&StatusTypes::RunLeft);
-                statuses.value.push(StatusTypes::Deceleration);
-            }
-            if keyboard_input.just_released(KeyCode::D) {
-                statuses.remove_status(&StatusTypes::RunRight);
-                statuses.value.push(StatusTypes::Deceleration);
-            }
-        }
-
-        statuses.value.dedup();
-    }
-}
-
-// fn gamepad_system(
-//     gamepads: Res<Gamepads>,
-//     button_inputs: Res<Input<GamepadButton>>,
-//     button_axes: Res<Axis<GamepadButton>>,
-//     axes: Res<Axis<GamepadAxis>>,
-//     mut query: Query<(&Player, &mut Statuses, &mut ViewDirection, &mut Sprite)>,
-// ) {
-//     for gamepad in gamepads.iter().cloned() {
-//         for (player, mut statuses, mut view_direction, mut sprite) in &mut query
-//         {
-//             if button_inputs.just_pressed(GamepadButton {
-//                 gamepad,
-//                 button_type: GamepadButtonType::West,
-//             }) {
-//                 if statuses.value.contains(&StatusTypes::Jumping) {
-//                     return;
-//                 }
-
-//                 statuses.value.push(StatusTypes::Jumping);
-//             }
-
-//             if button_inputs.just_pressed(GamepadButton {
-//                 gamepad,
-//                 button_type: GamepadButtonType::North,
-//             }) {
-//                 sprite.flip_x = false;
-//                 view_direction.value = ViewDirections::Left;
-//                 statuses.value.push(StatusTypes::RunLeft);
-//             }
-//             if button_inputs.just_pressed(GamepadButton {
-//                 gamepad,
-//                 button_type: GamepadButtonType::East,
-//             }) {
-//                 sprite.flip_x = true;
-//                 view_direction.value = ViewDirections::Right;
-//                 statuses.value.push(StatusTypes::RunRight);
-//             }
-
-//             if button_inputs.just_released(GamepadButton {
-//                 gamepad,
-//                 button_type: GamepadButtonType::North,
-//             }) {
-//                 statuses.remove_status(&StatusTypes::RunLeft);
-//                 statuses.value.push(StatusTypes::Deceleration);
-//             }
-
-//             if button_inputs.just_released(GamepadButton {
-//                 gamepad,
-//                 button_type: GamepadButtonType::East,
-//             }) {
-//                 statuses.remove_status(&StatusTypes::RunRight);
-//                 statuses.value.push(StatusTypes::Deceleration);
-//             }
-
-//             statuses.value.dedup();
-//         }
-//     }
-// }
-
-fn gamepad_system(
-    gamepads: Res<Gamepads>,
-    button_inputs: Res<Input<GamepadButton>>,
-    button_axes: Res<Axis<GamepadButton>>,
-    axes: Res<Axis<GamepadAxis>>,
-    mut query: Query<(&Player, &mut Statuses, &mut ViewDirection, &mut Sprite)>,
-) {
-    for gamepad in gamepads.iter().cloned() {
-        for (player, mut statuses, mut view_direction, mut sprite) in &mut query
-        {
-            if button_inputs.just_pressed(GamepadButton {
-                gamepad,
-                button_type: GamepadButtonType::West,
-            }) {
-                if statuses.value.contains(&StatusTypes::Jumping) {
-                    return;
-                }
-
-                statuses.value.push(StatusTypes::Jumping);
-            }
-
-            if button_inputs.just_pressed(GamepadButton {
-                gamepad,
-                button_type: GamepadButtonType::LeftTrigger,
-            }) {
-                sprite.flip_x = false;
-                view_direction.value = ViewDirections::Left;
-                statuses.value.push(StatusTypes::RunLeft);
-            }
-            if button_inputs.just_pressed(GamepadButton {
-                gamepad,
-                button_type: GamepadButtonType::RightTrigger,
-            }) {
-                sprite.flip_x = true;
-                view_direction.value = ViewDirections::Right;
-                statuses.value.push(StatusTypes::RunRight);
-            }
-
-            if button_inputs.just_released(GamepadButton {
-                gamepad,
-                button_type: GamepadButtonType::LeftTrigger,
-            }) {
-                statuses.remove_status(&StatusTypes::RunLeft);
-                statuses.value.push(StatusTypes::Deceleration);
-            }
-
-            if button_inputs.just_released(GamepadButton {
-                gamepad,
-                button_type: GamepadButtonType::RightTrigger,
-            }) {
-                statuses.remove_status(&StatusTypes::RunRight);
-                statuses.value.push(StatusTypes::Deceleration);
-            }
-
-            statuses.value.dedup();
-        }
-    }
-}
-
-fn move_camera(
-    mut query: Query<(Option<&Player>, Option<&Camera>, &mut Transform)>,
-) {
-    let mut entities = vec![];
-
-    for (player, camera, mut transform) in query.iter_mut() {
-        entities.push((player, camera, transform));
-    }
-
-    let mut player_translation: Vec3 = Vec3::default();
-
-    for entity in entities {
-        let player = entity.0;
-        let camera = entity.1;
-
-        // Get player translation
-        if let Some(_) = player {
-            let transform = entity.2;
-            player_translation = transform.translation;
-            continue;
-        }
-
-        // Move camera
-        if let Some(_) = camera {
-            let mut camera_transform = entity.2;
-            camera_transform.translation = player_translation;
-            continue;
-        }
-    }
 }
